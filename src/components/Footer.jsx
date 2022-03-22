085974a81e4202c51cc11bb6b15a728ad5f5f620
Vitaliy <43200383+s04v@users.noreply.github.com>
2022-03-22 14:47:31 +0100

    Main page creation completed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,33 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="footer-employee">
+                <h1>Employee</h1>
+                <ul>
+                    <li>Help</li>
+                    <li>CV creator</li>
+                    <li>Job Centers</li>
+                </ul>
+            </div>
+            <div className="footer-employer">
+                <h1>Employer</h1>
+                <ul>
+                    <li>Search Resume</li>
+                    <li>Employer Account</li>
+                    <li>Help for companies</li>
+                </ul>
+            </div>
+            <div className="footer-other">
+                <h1>Other menu</h1>
+                <ul>
+                    <li>Terms and Conditions</li>
+                    <li>Privacy Policy</li>
+                    <li>Cookies policy</li>
+                    <li>Cookies settings</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
