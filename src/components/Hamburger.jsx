791acd0ef70a4896b543d6083ef554b9b30503c6
aleh <oleg.hutsko@gmail.com>
2022-04-03 17:08:07 +0200

    fix returned data from SignForm

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -4,7 +4,7 @@ import style from '../styles/components/hamburger.module.scss'
 
 const Hamburger = ({onClick=null, className=null}) => {
   return (
-    <label for="check" onChange={onClick} className={className}>
+    <label htmlFor="check" onChange={onClick} className={className}>
       <input type="checkbox" id="check" />
       <span></span>
       <span></span>
--- src/components/SignForm.jsx
+++ src/components/SignForm.jsx
@@ -1,12 +1,21 @@
-import React, {useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from './Button';
 
 export const SignForm = ({title, fields, onSend, buttonTitle}) => {
-    const [data, setData] = useState({});
+  const [data, setData] = useState({});
 
-    const onChange = (e) => {
-        setData({...data, [e.target.name]:e.target.value});
-    }
+  const onChange = (e) => {
+      setData({...data, [e.target.name]:e.target.value});
+  }
+
+  useEffect(() => {
+    const tempData = {};
+    fields.forEach(element => {
+      tempData[element.name] = '';
+    });
+    setData(tempData);
+    return () => setData({});
+  }, [JSON.stringify(fields)]);
 
   return (
     <div className='form-section'>
@@ -14,7 +23,7 @@ export const SignForm = ({title, fields, onSend, buttonTitle}) => {
       {fields.map((item) =>
         <div key={item.id}>
           <p>{item.title}</p>
-          <input type={item.type} name={item.name} onChange={onChange}/>
+          <input type={item.type} value={data[item.name]} name={item.name} onChange={onChange}/>
         </div>
       )}
       <Button onClick={() => onSend(data)} text={buttonTitle}/>
