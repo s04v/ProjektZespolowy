31edb95cf156662c0bf285f6e165c55ccfde633a
dzmanchak <63239337+dzmanchak@users.noreply.github.com>
2022-04-09 18:39:15 +0200

    Revert "Revert "User singin/singuo""
    This reverts commit bd5b16c19da0c448e2aa0a73db99b88d6fcba796.

---
+++ Services/UserService.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using FindJobWebApi.DataBase;
+using FindJobWebApi.DTOs;
+using FindJobWebApi.Models;
+using FindJobWebApi.Secure;
+
+namespace FindJobWebApi.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly AppDBContext _context;
+        private readonly IMapper _mapper;
+
+        public UserService(AppDBContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+
+        }
+
+        public string SignIn(LoginUserDTO userDTO)
+        {
+           var currentUser = _context.Users.SingleOrDefault(x => x.Email.Equals(userDTO.Email));
+           var hashedPassword = userDTO.Password.getHash();
+            if (currentUser == null || !hashedPassword.Equals(currentUser.Password))
+            {
+                return "Login/Password is incorrect!";
+            }
+            return currentUser.Id.ToString();
+        }
+
+        public string SignUp(CreateUserDTO userDTO)
+        {
+            if (_context.Users.Any(x => x.Email == userDTO.Email))
+            {
+                return "Account with this email address already exists!";
+            }
+
+            var currentUser = _mapper.Map<User>(userDTO);
+            currentUser.Password = userDTO.Password.getHash();
+            _context.Users.Add(currentUser);
+            _context.SaveChanges();
+            return "OK";
+        }
+    }
+}
