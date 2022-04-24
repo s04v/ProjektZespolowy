f47be9ed635f796064667c33510f7f5b7c563686
dzmanchak <63239337+dzmanchak@users.noreply.github.com>
2022-04-24 12:29:28 +0200

    Added UserDTO / UserAdressDTO

---
+++ DTOs/UserDTOcs.cs
@@ -0,0 +1,17 @@
+﻿namespace FindJobWebApi.DTOs
+{
+    public class UserDTOcs
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? BirthdayDate { get; set; }
+        public string? ContactNumber { get; set; }
+        public virtual UserAddressDTO UserAddress { get; set; }
+        public string? Gender { get; set; }
+        public float? Experience { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string? Desciption { get; set; }
+    }
+}
