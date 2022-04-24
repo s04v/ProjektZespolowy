c822ceaa4d4a316cf7b0e3937ea6f3c4b048d2b8
dzmanchak <63239337+dzmanchak@users.noreply.github.com>
2022-04-24 14:35:00 +0200

    Updated VacancyService, Implemented AddNewVacancy

--- Services/VacancyService.cs
+++ Services/VacancyService.cs
@@ -1,12 +1,31 @@
-﻿using FindJobWebApi.DTOs;
+﻿using AutoMapper;
+using FindJobWebApi.DataBase;
+using FindJobWebApi.DTOs;
+using FindJobWebApi.Models;
 
 namespace FindJobWebApi.Services
 {
     public class VacancyService : IVacancyService
     {
+        private readonly AppDBContext _context;
+        private readonly IMapper _mapper;
+
+        public VacancyService(AppDBContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
         public string AddNewVacancy(CreateVacancyDTO vacancyDTO)
         {
-            throw new NotImplementedException();
+            var currentVacancy = _mapper.Map<Vacancy>(vacancyDTO);
+
+            currentVacancy.UpdateTime = DateTime.Now;
+
+            _context.Vacancies.Add(currentVacancy);
+            _context.SaveChanges();
+
+            return "OK";
         }
     }
 }
