79bb1cf1645a2ba7a832d79a56861689a625e672
dzmanchak <63239337+dzmanchak@users.noreply.github.com>
2022-04-24 14:28:10 +0200

    Added IVacancyService/VacancyService

---
+++ Services/VacancyService.cs
@@ -0,0 +1,12 @@
+﻿using FindJobWebApi.DTOs;
+
+namespace FindJobWebApi.Services
+{
+    public class VacancyService : IVacancyService
+    {
+        public string AddNewVacancy(CreateVacancyDTO vacancyDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
