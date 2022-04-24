4088864e7c9bf0ae7a7218801301c387af23dfe4
aleh <oleg.hutsko@gmail.com>
2022-04-24 21:45:17 +0200

    add row component

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-    baseURL: 'https://pz-api-findjob.herokuapp.com/',
+    baseURL: 'https://localhost:7253/',
     timeout: 1000,
     withCredentials: true,
 });
--- src/components/OfferChanger.jsx
+++ src/components/OfferChanger.jsx
@@ -32,7 +32,7 @@ const OfferChanger = () => {
     ]
     return (
         <div className={style.offerChanger} >
-            <div className={[style.header, opened].join(" ")}  >
+            <div className={[style.header, style.no_rows, opened].join(" ")}  >
                 <h2>Job offers</h2>
                 <AiOutlinePlus size={50} onClick={() => setOfferFields(!offerFields)}/>
             </div>
@@ -42,6 +42,9 @@ const OfferChanger = () => {
                     onSend={onSend}
                     buttonTitle='Save' />
             </div>
+            <OfferRow />
+            <OfferRow />
+            <OfferRow />
             <OfferRow />
         </div>
     )
