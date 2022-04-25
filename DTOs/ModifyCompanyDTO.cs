4d16e62b871834d3e9b64588f4c7edb0d7b7352f
dzmanchak <63239337+dzmanchak@users.noreply.github.com>
2022-04-26 00:38:23 +0200

    Fixed ModifyCompanyDTO

--- DTOs/ModifyCompanyDTO.cs
+++ DTOs/ModifyCompanyDTO.cs
@@ -2,10 +2,10 @@
 {
     public class ModifyCompanyDTO
     {
-        public string CompanyName { get; set; }
-        public int CompanyAddressId { get; set; }
-        public string Website { get; set; }
-        public string Password { get; set; }
-        public string Desciption { get; set; }
+        public string? CompanyName { get; set; }
+        public int? CompanyAddressId { get; set; }
+        public string? Website { get; set; }
+        public string? Password { get; set; }
+        public string? Desciption { get; set; }
     }
 }
